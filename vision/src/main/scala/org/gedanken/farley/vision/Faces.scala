a3ab00dc21fcc7037f65517cd4ac55cf3e8f1b79
Logan O'Sullivan Bruns <logan@gedanken.org>
2015-02-11 15:22:47 +0000

    Fixed a comment.

--- vision/src/main/scala/org/gedanken/farley/vision/Faces.scala
+++ vision/src/main/scala/org/gedanken/farley/vision/Faces.scala
@@ -4,7 +4,7 @@ package org.gedanken.org.farley.vision
   * 
   * vision/Faces.scala
   * 
-  * Copyright 2013, 2014, 2015 Logan O'Sullivan Bruns
+  * Copyright 2015 Logan O'Sullivan Bruns
   * 
   * Licensed under the Apache License, Version 2.0 (the "License");
   * you may not use this file except in compliance with the License.
