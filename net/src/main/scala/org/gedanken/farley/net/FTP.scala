f5c2a81d0a492d7a72cb3d272a091fd4eb88a4c0
Logan O'Sullivan Bruns <logan@gedanken.org>
2015-02-11 15:20:35 +0000

    Started work on some net utilities
    At least initially to make it easier to run operations on large sets
    of images and videos hosted elsewhere.

---
+++ net/src/main/scala/org/gedanken/farley/net/FTP.scala
@@ -0,0 +1,45 @@
+package org.gedanken.org.farley.net
+
+import org.apache.commons.net.ftp.FTPClient
+
+/**
+  * 
+  * net/FTP.scala
+  * 
+  * Copyright 2015 Logan O'Sullivan Bruns
+  * 
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  * 
+  *  http://www.apache.org/licenses/LICENSE-2.0
+  * 
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+class FTP(hostname: String) {
+
+  val client = new FTPClient()
+
+  client.connect(hostname)
+
+  client.enterLocalPassiveMode
+
+  def login(username: String, password: String) = 
+    client.login(username, password)
+
+  def close() = {
+    if (client.isConnected)
+      client.disconnect
+  }
+
+  def listFiles = 
+    client.listFiles
+
+  def listFiles(directory: String) = 
+    client.listFiles(directory)
+}
