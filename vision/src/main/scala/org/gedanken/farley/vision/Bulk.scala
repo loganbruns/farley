7b51e168570a434593281cb1b3abe8796c5cfa7b
Logan O'Sullivan Bruns <logan@gedanken.org>
2015-02-15 22:58:36 +0000

    Added a bulk facial detection utility
    Scans large volumes of images hosted on FTP sites for faces in
    parallel by trying several filters and orientations.

---
+++ vision/src/main/scala/org/gedanken/farley/vision/Bulk.scala
@@ -0,0 +1,46 @@
+package org.gedanken.org.farley.vision
+
+/**
+  * 
+  * vision/Bulk.scala
+  * 
+  * Copyright 2015 Logan O'Sullivan Bruns
+  * 
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  * 
+  *  http://www.apache.org/licenses/LICENSE-2.0
+  * 
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+import org.apache.commons.net.ftp.FTPFile
+import org.gedanken.org.farley.net.FTP
+import org.opencv.core._
+
+object Bulk {
+
+  /** Filter images in bulk from an FTP site */
+  def filter(
+    ftp: FTP,
+    directory: String,
+    images: Seq[FTPFile]
+  ) : Seq[FTPFile] = {
+    images.par.filter((file : FTPFile) => {
+      val image = Faces.load(ftp.retrieve(directory + "/" + file.getName))
+      val flippedImage = new Mat
+
+      Core.flip(image, flippedImage, 0)
+      
+      (Faces.detect(image, Faces.FRONTAL_FACE).toArray.length > 0) ||
+      (Faces.detect(image, Faces.RIGHT_PROFILE_FACE).toArray.length > 0) ||
+      (Faces.detect(flippedImage, Faces.FRONTAL_FACE).toArray.length > 0) ||
+      (Faces.detect(flippedImage, Faces.RIGHT_PROFILE_FACE).toArray.length > 0)
+    }).seq
+  }
+}
